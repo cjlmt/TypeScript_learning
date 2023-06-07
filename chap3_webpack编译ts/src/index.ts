024c863c9c2b27923ce4651e316a2cb4c9ce0319
cjlmt <604495355@qq.com>
2023-06-07 21:49:31 +0800

    Compiled typescript to js with webpack

---
+++ chap3_webpack编译ts/src/index.ts
@@ -0,0 +1,4 @@
+function func(a: number, b: number): number {
+    return a + b
+}
+console.log(func(123, 456));
---
+++ chap3_webpack编译ts/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+//nodejs里的一个模块，帮助拼接路径
+module.exports = {
+    //指定项目众多文件中的入口文件。源码一般放在src目录下
+    entry: "./src/index.ts",
+    //指定打包后的文件存放位置
+    output: {
+        //指定打包文件的目录
+        path: path.resolve(__dirname, "dist"), // 把路径完整地拼出来
+        //打包后文件的名字
+        filename: "bundle.js",
+    },
+    //指定webpack打包时要使用的模块
+    module: {
+        //指定要加载的规则
+        rules: [
+            {
+                //指定规则生效的文件
+                test: /\.ts$/,  //使用正则表达式，匹配所有以ts结尾的文件进行编译
+                //要使用的loader
+                use: {
+                    loader: "ts-loader"
+                },
+                //不参与编译的文件
+                exclude: /node_modules/
+            }
+        ]
+    },
+}
